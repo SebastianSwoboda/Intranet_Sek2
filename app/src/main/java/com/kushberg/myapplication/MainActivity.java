a77adad2db8f34aa7056a31a9aa8e8a1f44ee230
JoelRauber <35917298+JoelRauber@users.noreply.github.com>
2018-09-11 12:48:05 +0200

    temporary listView added to mainActivity

--- app/src/main/java/com/kushberg/myapplication/MainActivity.java
+++ app/src/main/java/com/kushberg/myapplication/MainActivity.java
@@ -2,13 +2,33 @@ package com.kushberg.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView timetableList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timetableList = (ListView)findViewById(R.id.timetableList);
+
+        List<String> subjects = new ArrayList<String>();
+        subjects.add("Test");
+        subjects.add("Test");
+        subjects.add("Test");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                subjects );
+
+        timetableList.setAdapter(adapter);
     }
 
 
