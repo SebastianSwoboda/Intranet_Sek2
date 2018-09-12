e742c5aca43beddda90dd3396242a3d277a514b5
JoelRauber <35917298+JoelRauber@users.noreply.github.com>
2018-09-12 13:31:23 +0200

    Jsoup login does not work even though connection is running in a new thread. I fucking hate android.

---
+++ app/src/main/java/com/kushberg/myapplication/Connection.java
@@ -0,0 +1,90 @@
+package com.kushberg.myapplication;
+
+import android.os.AsyncTask;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Connection extends AsyncTask<Void,Void, Void> {
+    
+    
+
+    final String USER_AGENT = "Mozilla";
+
+    String loginFormUrl = "https://intranet.tam.ch/";
+    String loginActionUrl = "https://intranet.tam.ch/";
+    String timeTableUrl = "https://intranet.tam.ch/tbz/calendar";
+    String loginuser = "sebastian.swoboda@edu.tbz.ch";
+    String loginpassword = "Sebolanawurscht9";
+    String loginschool = "tbz";
+
+    Map<String, String> cookies = new HashMap<>();
+    Map<String, String> formData = new HashMap<>();
+    Map<String, String> timeTableAccess = new HashMap<>();
+
+    String finalUrl;
+
+
+
+    @Override
+    protected Void doInBackground(Void... arg0)   {
+
+        try {
+
+        org.jsoup.Connection.Response loginForm = Jsoup.connect(loginFormUrl).method(org.jsoup.Connection.Method.GET).userAgent(USER_AGENT).execute();
+        Document loginDoc = loginForm.parse();
+
+        cookies.putAll(loginForm.cookies());
+
+
+
+
+
+
+        formData.put("Button","Anmelden");
+        formData.put("utf8","e2 9c 93");
+        formData.put("loginuser",loginuser);
+        formData.put("loginpassword",loginpassword);
+        formData.put("loginschool",loginschool);
+
+        org.jsoup.Connection.Response homePage = null;
+
+            homePage = Jsoup.connect(loginActionUrl)
+                    .cookies(cookies)
+                    .data(formData)
+                    .method(org.jsoup.Connection.Method.POST)
+                    .userAgent(USER_AGENT)
+
+                    .execute();
+
+
+        cookies.putAll(homePage.cookies());
+
+        timeTableAccess.put("menuitem","Stundenplan");
+
+        org.jsoup.Connection.Response timetablePage = Jsoup.connect(timeTableUrl)
+                .cookies(cookies)
+                .data(timeTableAccess)
+                .method(org.jsoup.Connection.Method.GET)
+                .userAgent(USER_AGENT)
+                .execute();
+
+             finalUrl = timetablePage.url().toString();
+
+
+
+        System.out.println(timetablePage.parse().html());
+        Document timeTableDocument = timetablePage.parse();
+        return null;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
--- app/src/main/java/com/kushberg/myapplication/LoginActivity.java
+++ app/src/main/java/com/kushberg/myapplication/LoginActivity.java
@@ -2,6 +2,8 @@ package com.kushberg.myapplication;
 
 
 import android.content.Intent;
+import android.os.StrictMode.ThreadPolicy;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.widget.EditText;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.jsoup.Connection;
@@ -20,8 +23,15 @@ import org.jsoup.nodes.Document;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String finalUrl;
+
+    com.kushberg.myapplication.Connection connection = new com.kushberg.myapplication.Connection();
+
+    /*
     String password;
     String username;
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +49,32 @@ public class LoginActivity extends AppCompatActivity {
                 {
                     public void onClick(View view)
                     {
-                          username = emailField.getText().toString();
+                        /*
+                        username = emailField.getText().toString();
 
-                         password = passwordField.getText().toString() ;
+                        password = passwordField.getText().toString() ;
+                        */
+
+                            connection.doInBackground();
 
 
-                        startActivity(intent);
+
+                         if (connection.finalUrl.equals("https://intranet.tam.ch/tbz/calendar?menuitem=Stundenplan")){
+                             startActivity(intent);
+
+                         }else{
+                            System.out.println("You fucked up");
+
+                         }
+
+
+
                     }
                 });
+
+
+
+
 
 
 
@@ -54,6 +82,87 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+
+
+
+/*
+    public void test(){
+
+        final String USER_AGENT = "Mozilla";
+
+        String loginFormUrl = "https://intranet.tam.ch/";
+        String loginActionUrl = "https://intranet.tam.ch/";
+        String timeTableUrl = "https://intranet.tam.ch/tbz/calendar";
+        String loginuser = "sebastian.swoboda@edu.tbz.ch";
+        String loginpassword = "Sebolanawurscht9";
+        String loginschool = "tbz";
+
+        Map<String, String> cookies = new HashMap<>();
+        Map<String, String> formData = new HashMap<>();
+        Map<String, String> timeTableAccess = new HashMap<>();
+
+
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    org.jsoup.Connection.Response loginForm = Jsoup.connect(loginFormUrl).method(org.jsoup.Connection.Method.GET).userAgent(USER_AGENT).execute();
+                    Document loginDoc = loginForm.parse();
+
+                    cookies.putAll(loginForm.cookies());
+
+
+
+
+
+
+                    formData.put("Button","Anmelden");
+                    formData.put("utf8","e2 9c 93");
+                    formData.put("loginuser",loginuser);
+                    formData.put("loginpassword",loginpassword);
+                    formData.put("loginschool",loginschool);
+
+                    org.jsoup.Connection.Response homePage = null;
+
+                    homePage = Jsoup.connect(loginActionUrl)
+                            .cookies(cookies)
+                            .data(formData)
+                            .method(org.jsoup.Connection.Method.POST)
+                            .userAgent(USER_AGENT)
+
+                            .execute();
+
+
+                    cookies.putAll(homePage.cookies());
+
+                    timeTableAccess.put("menuitem","Stundenplan");
+
+                    org.jsoup.Connection.Response timetablePage = Jsoup.connect(timeTableUrl)
+                            .cookies(cookies)
+                            .data(timeTableAccess)
+                            .method(org.jsoup.Connection.Method.GET)
+                            .userAgent(USER_AGENT)
+                            .execute();
+
+                    finalUrl = timetablePage.url().toString();
+
+
+
+                    System.out.println(timetablePage.parse().html());
+                    Document timeTableDocument = timetablePage.parse();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        thread.start();
+    }*/
 
 
 
